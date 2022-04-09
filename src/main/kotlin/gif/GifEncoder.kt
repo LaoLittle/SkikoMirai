f4da793158bb478ab5bf6bd1ebbbc69f094727ea
LaoLittle <y1941803875@outlook.com>
2022-04-09 23:50:42 +0800

    update

--- src/main/kotlin/gif/GifEncoder.kt
+++ src/main/kotlin/gif/GifEncoder.kt
@@ -8,8 +8,6 @@ public class GifEncoder(ptr: LongArray) {
     public operator fun component1(): Collector = collector
     public operator fun component2(): Writer = writer
 
-    public suspend fun encodeToByteArray(): ByteArray = writer.writeToBytes()
-
     public companion object {
         public fun new(setting: GifSetting): GifEncoder =
             GifEncoder(nNewEncoder(setting.width, setting.height, setting.quality, setting.fast, setting.repeat.times))
--- src/main/kotlin/gif/GifImage.kt
+++ src/main/kotlin/gif/GifImage.kt
@@ -1,4 +1,32 @@
 package org.laolittle.plugin.gif
 
-public class GifImage {
+import java.util.ArrayList
+
+public class GifImage(private val images: Array<ByteArray>,public val bytes: ByteArray) {
+    public val frameCount: Int get() = images.size
+
+    public val frames: Array<ByteArray> get() = images
 }
+
+public class GifImageBuilder(setting: GifSetting = GifSetting.default()) {
+    private var frameIndex = 0
+
+    public val encoder: GifEncoder = GifEncoder.new(setting)
+
+    private val images: ArrayList<Pair<ByteArray, Double>> = arrayListOf()
+
+    public fun addFrame(image: ByteArray, delay: Double = 1.0): Boolean = images.add(image to delay)
+
+    public suspend fun build(): GifImage {
+        val (collector, writer) = encoder
+        for (image in images) {
+            collector.addFrame(image.first, frameIndex++, image.second)
+        }
+        collector.close()
+
+        return GifImage(images.map { it.first }.toTypedArray(), writer.writeToBytes())
+    }
+}
+
+public suspend fun buildGifImage(setting: GifSetting, block: GifImageBuilder.() -> Unit): GifImage =
+    GifImageBuilder(setting).apply(block).build()
