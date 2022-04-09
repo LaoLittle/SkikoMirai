fc8d4a339d5e97efecfd20938107b8ee61b3ece7
LaoLittle <y1941803875@outlook.com>
2022-04-09 23:00:41 +0800

    update

--- src/main/kotlin/gif/GifLibrary.kt
+++ src/main/kotlin/gif/GifLibrary.kt
@@ -4,12 +4,13 @@ import org.laolittle.plugin.DefaultNativeLibFolder
 
 public object GifLibrary {
     internal var loaded = false
-    private set
+        private set
+
     private val defaultGifLibrary = DefaultNativeLibFolder.resolve(System.mapLibraryName("gifski"))
 
     public fun load() {
-        loaded = true
-
         System.load(defaultGifLibrary.absolutePath)
+
+        loaded = true
     }
 }
---
+++ src/main/rust/gifski_jni.rs
@@ -0,0 +1,110 @@
+use std::borrow::Cow;
+use std::fs::File;
+use std::ptr::slice_from_raw_parts;
+use imgref::Img;
+use jni::JNIEnv;
+use jni::objects::{JClass, ReleaseMode};
+use jni::sys::{jboolean, jbyte, jbyteArray, jdouble, jint, jlong, jlongArray, JNI_TRUE, jshort, jstring};
+use rgb::RGBA8;
+use crate::{Collector, new, NoProgress, Repeat, Settings, Writer};
+
+#[no_mangle]
+pub extern "system" fn Java_org_laolittle_plugin_gif_GifEncoderKt_nNewEncoder(
+    env: JNIEnv,
+    _class: JClass,
+    width: jint,
+    height: jint,
+    quality: jbyte,
+    fast: jboolean,
+    repeat: jshort,
+) -> jlongArray {
+    let setting = Settings {
+        width: if width <= 0 { None } else { Some(width as u32) },
+        height: if height <= 0 { None } else { Some(height as u32) },
+        quality: quality as u8,
+        fast: fast == JNI_TRUE,
+        repeat: if repeat >=0 { Repeat::Finite(repeat as u16) } else { Repeat::Infinite }
+    };
+
+    let (c, w) = new(setting).unwrap();
+
+    let c_raw = Box::into_raw(c.into());
+    let w_raw = Box::into_raw(w.into());
+
+    let ptr = env.new_long_array(2).unwrap();
+
+    env.set_long_array_region(ptr, 0, &[raw_ptr_to_long(c_raw), raw_ptr_to_long(w_raw)]).unwrap();
+    ptr
+}
+
+#[no_mangle]
+pub extern "system" fn Java_org_laolittle_plugin_gif_CollectorKt_nCollectorAddFrameBytes(
+    env: JNIEnv,
+    _class: JClass,
+    bytes: jbyteArray,
+    frame_index: jint,
+    presentation: jdouble,
+    ptr: jlong,
+) -> i64 {
+    let mut collector = *unsafe { Box::<Collector>::from_raw(long_to_raw_ptr(ptr)) };
+
+    let len = env.get_array_length(bytes).unwrap();
+    let primitive = env.get_primitive_array_critical(bytes, ReleaseMode::CopyBack).unwrap();
+    let data = unsafe { &*slice_from_raw_parts(primitive.as_ptr() as *const u8, len as usize) };
+
+    if let Ok(bitmap) = lodepng::decode32(data) {
+        let image: Img<Cow<[RGBA8]>> = Img::new(bitmap.buffer.into(), bitmap.width, bitmap.height);
+        collector.add_frame_rgba_cow(frame_index as usize, image, presentation).unwrap();
+    }
+
+    let raw = Box::into_raw(collector.into());
+    raw_ptr_to_long(raw)
+}
+
+#[no_mangle]
+pub extern "system" fn Java_org_laolittle_plugin_gif_CollectorKt_nCloseCollector(
+    _env: JNIEnv,
+    _class: JClass,
+    ptr: jlong,
+) {
+    unsafe { Box::<Collector>::from_raw(long_to_raw_ptr(ptr)) };
+}
+
+#[no_mangle]
+pub extern "system" fn Java_org_laolittle_plugin_gif_WriterKt_nWriteToFile(
+    env: JNIEnv,
+    _class: JClass,
+    path: jstring,
+    writer: jlong,
+) {
+    let writer = unsafe { Box::<Writer>::from_raw(long_to_raw_ptr(writer)) };
+    let str = env.get_string(path.into()).unwrap();
+    let path = str.to_str().unwrap();
+    let f = File::create(path).unwrap();
+
+    let mut nop = NoProgress {};
+    writer.write(f, &mut nop).unwrap();
+}
+
+#[no_mangle]
+pub extern "system" fn Java_org_laolittle_plugin_gif_WriterKt_nWriteToBytes(
+    env: JNIEnv,
+    _class: JClass,
+    writer: jlong,
+) -> jbyteArray {
+    let writer = unsafe { Box::<Writer>::from_raw(long_to_raw_ptr(writer)) };
+    let mut buff: Vec<u8> = Vec::with_capacity(4096);
+
+    let mut nop = NoProgress {};
+    writer.write(&mut buff, &mut nop).unwrap();
+
+    env.byte_array_from_slice(&buff).unwrap()
+}
+
+fn raw_ptr_to_long<T>(ptr: *const T) -> i64 {
+    ptr as usize as i64
+}
+
+fn long_to_raw_ptr<T>(value: i64) -> *mut T {
+    value as usize as *mut T
+}
