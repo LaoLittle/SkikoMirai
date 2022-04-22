01b02856fe8313e8c96932a6d25f3e948ba5fe9b
LaoLittle <y1941803875@outlook.com>
2022-04-22 18:12:44 +0800

    update

--- src/main/kotlin/SkikoMirai.kt
+++ src/main/kotlin/SkikoMirai.kt
@@ -17,7 +17,7 @@ public object SkikoMirai : KotlinPlugin(
     JvmPluginDescription(
         id = "org.laolittle.plugin.SkikoMirai",
         name = "SkikoMirai",
-        version = "1.0.6",
+        version = "1.0.7",
     ) {
         author("LaoLittle")
     }
